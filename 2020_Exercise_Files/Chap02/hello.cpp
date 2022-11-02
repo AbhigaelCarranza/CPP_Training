cf928499789c499a8989d872b7f423985a7ad39b
AbhigaelCarranza <abhicarranz@gmail.com>
2022-11-02 09:56:00 -0700

    News

--- 2020_Exercise_Files/Chap02/hello.cpp
+++ 2020_Exercise_Files/Chap02/hello.cpp
@@ -1,8 +1,19 @@
 // hello.cpp by Bill Weinman [bw.org]
 // updated 2020-06-24
 #include <cstdio>
+#include <string>
+using namespace std;
+
+string SnakeCase(string str) {
+  
+  // code goes here 
+   
+  return str;
+
+}
 
 int main() {
-    puts("Hello, World!");
+    
+    cout << SnakeCase("cats AND*Dogs-are Awesome");
     return 0;
 }
